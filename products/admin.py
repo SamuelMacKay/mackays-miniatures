ad18f5fd859a55f09141455da844bc08db802d29
Samuel Mackay <samuel.john.mackay@gmail.com>
2024-10-28 14:18:28 +0000

    MODIFY: Customised admin

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,23 @@ from django.contrib import admin
 from .models import Product, Faction
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Faction)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'faction',
+        'price',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class FactionAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Faction, FactionAdmin)
