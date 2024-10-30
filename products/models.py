2292b89b4c68a4773897456511767f17ac4f56fb
Samuel Mackay <samuel.john.mackay@gmail.com>
2024-10-30 10:40:58 +0000

    ADDED: Category filtering and new fields to products

--- products/models.py
+++ products/models.py
@@ -19,6 +19,8 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    is_new = models.BooleanField(default=False)
+    pre_order = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,  redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Faction
 
 # Create your views here.
 
@@ -10,20 +10,29 @@ def all_products(request):
     
     products = Product.objects.all()
     query = None
+    factions = None
 
     if request.GET:
-        if 'q' in request.GET:
-            query = request.GET['q']
-            if not query:
-                messages.error(request, "You didn't enter any search criteria!")
-                return redirect(reverse('products'))
-            
-            queries = Q(name__icontains=query) | Q(setting__icontains=query)
-            products = products.filter(queries)
+        if 'faction' in request.GET:
+            factions = request.GET['faction'].split(',')
+            products = products.filter(faction__name__in=factions)
+            factions = Faction.objects.filter(name__in=factions)
+
+
+        if request.GET:
+            if 'q' in request.GET:
+                query = request.GET['q']
+                if not query:
+                    messages.error(request, "You didn't enter any search criteria!")
+                    return redirect(reverse('products'))
+                
+                queries = Q(name__icontains=query) | Q(setting__icontains=query)
+                products = products.filter(queries)
 
     context = {
         'products': products,
         'search_term': query,
+        'current_factions': factions
     }
 
     return render(request, 'products/products.html', context)
