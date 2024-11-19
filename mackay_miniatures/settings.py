51e306308b9b41094095e09be011398db165badb
Samuel Mackay <samuel.john.mackay@gmail.com>
2024-11-19 16:54:11 +0000

    FIX: set up SECRET_KEY and testing if heroku app is connected properly

--- mackay_miniatures/settings.py
+++ mackay_miniatures/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%wqm9&&&jc38r%=69jl)q7m%u^axum82kdqk!!vsi%%z!o--60'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
