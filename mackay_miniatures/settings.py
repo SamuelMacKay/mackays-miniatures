10aadf4ad06af5a3ff53b3c141fc1136b8ec761b
Samuel Mackay <samuel.john.mackay@gmail.com>
2024-11-20 20:20:19 +0000

    FIX: Fixing emails and debug settings in env

--- mackay_miniatures/settings.py
+++ mackay_miniatures/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEVELOPMENT')
+DEBUG = os.environ.get("DEVELOPMENT")
 
 ALLOWED_HOSTS = ['mackays-miniatures-sam-m-29767862f4e8.herokuapp.com', '8000-samuelmacka-mackaysmini-od5sa4qu8rf.ws.codeinstitute-ide.net']
 
